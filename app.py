c156b79096715dcfde640ecd64167143b3ed04e1
AliBazmi28 <alibazmi28@gmail.com>
2025-06-12 16:25:37 +0500

    updated code added printing env

--- app.py
+++ app.py
@@ -6,6 +6,28 @@ import mysql.connector
 app = Flask(__name__)
 CORS(app)
 
+db_host = os.environ.get('DB_HOST')
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
+db_name = os.environ.get('DB_NAME')
+db_port = os.environ.get('DB_PORT')
+
+print("DB_HOST:", db_host)
+print("DB_USER:", db_user)
+print("DB_NAME:", db_name)
+print("DB_PORT:", db_port)
+
+if not all([db_host, db_user, db_password, db_name, db_port]):
+    raise Exception("One or more DB environment variables are missing.")
+
+db = mysql.connector.connect(
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_name,
+    port=int(db_port)
+)
+
 db = mysql.connector.connect(
     host=os.environ['DB_HOST'],
     user=os.environ['DB_USER'],
